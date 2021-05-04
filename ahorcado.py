f2d95ce7c1000041fb970d0296fc68de5b7ba7d5
Carlos Rivas <carb2297@gmail.com>
2021-05-04 12:03:56 -0500

    Se agrego detection de palabras y solo ingresar letras

--- ahorcado.py
+++ ahorcado.py
@@ -17,10 +17,48 @@ def choose_word(word_list):
     word_choosed = word_list[k]
     return(word_choosed)
 
+def input_letter():
+    try:
+        letter = input("Ingresa una letra: ")
+        if letter.isnumeric() == True:
+            raise ValueError("Solo puedes ingresar letras")
+        return letter
+    
+    except ValueError as ve:
+        print(ve)
+        return False
+
+def letter_compared(original, new, new_word):  
+    if new in original:  #detecta si la letra ingresada esta en la palabra
+        i = 0
+        while i < len(original):  #ubica la letra ingresada en la palabra nueva
+            if new == original[i]:
+                new_word[i] = new
+            i = i+1
+        return new_word
+    else:
+        print('Buen intento')
+        return False
+
 def run():
     a = read_data()
     word = choose_word(a)
-    print(word)
-
+    secret_word = [i for i in word]
+    #print(secret_word)
+    b = len(secret_word)
+    
+    i = 0
+    new_list = ['' for i in range(0,b)]
+    print(new_list)
+    while i <= b:
+        new_letter = input_letter()
+        if new_letter !=False:
+            new_list = letter_compared(secret_word, new_letter, new_list)
+            i = i+1
+            print(new_list)
+        if not '' in new_list:
+            print('ganaste')
+            break 
+    
 if __name__ == '__main__':
     run()
