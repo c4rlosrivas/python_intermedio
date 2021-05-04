f0b28e338d7997b425921a2ad9e6878f20aa61f0
Carlos Rivas <carb2297@gmail.com>
2021-05-04 14:59:10 -0500

    Deteccion de tildes y debug

--- ahorcado.py
+++ ahorcado.py
@@ -11,22 +11,26 @@ def read_data():
     
     return(words)
 
+
 def choose_word(word_list):
     len_list = len(word_list)
     k = random.randint(0, len_list-1)
     word_choosed = word_list[k]
     return(word_choosed)
 
+
 def input_letter():
     try:
         letter = input("Ingresa una letra: ")
-        if letter.isnumeric() == True:
-            raise ValueError("Solo puedes ingresar letras")
+
+        if letter.isnumeric() == True or len(letter) > 1:
+            raise ValueError("No ingresar numeros ni espacios")
         return letter
     
     except ValueError as ve:
         print(ve)
         return False
+
 
 def letter_compared(original, new, new_word):  
     if new in original:  #detecta si la letra ingresada esta en la palabra
@@ -38,13 +42,18 @@ def letter_compared(original, new, new_word):
         return new_word
     else:
         print('Buen intento')
-        return False
+        return new_word
+
 
 def run():
     a = read_data()
     word = choose_word(a)
     secret_word = [i for i in word]
-    #print(secret_word)
+    print(secret_word)
+    
+    if ('á' in secret_word) or ('é' in secret_word) or ('í'in secret_word) or ('ó'in secret_word) or ('ú' in secret_word):
+        print('En la palabra hay una tilde')
+
     b = len(secret_word)
     
     i = 0
@@ -55,10 +64,12 @@ def run():
         if new_letter !=False:
             new_list = letter_compared(secret_word, new_letter, new_list)
             i = i+1
+            
             print(new_list)
-        if not '' in new_list:
-            print('ganaste')
-            break 
+    
+            if not '' in new_list:
+                print('ganaste')
+                break 
     
 if __name__ == '__main__':
     run()
