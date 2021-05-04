a592a624857bcc8ace2175c0b0be22591d2b227b
Carlos Rivas <carb2297@gmail.com>
2021-05-04 10:26:33 -0500

    Proyect Hangman Game - choose random word

--- ahorcado.py
+++ ahorcado.py
@@ -1,1 +1,26 @@
+
 #aqui va ir el juego del ahorcado
+import random
+
+def read_data():
+    words = []
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.replace("\n", "") #elimina salto de linea "\n"
+            words.append(line)
+    
+    return(words)
+
+def choose_word(word_list):
+    len_list = len(word_list)
+    k = random.randint(0, len_list-1)
+    word_choosed = word_list[k]
+    return(word_choosed)
+
+def run():
+    a = read_data()
+    word = choose_word(a)
+    print(word)
+
+if __name__ == '__main__':
+    run()
